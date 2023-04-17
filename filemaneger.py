7586ed8ccab18e3738c5e0e46927db543e460cc6
yasya110865 <evn04@mail.ru>
2023-04-17 20:08:31 +0600

    new dz

---
+++ filemaneger.py
@@ -0,0 +1,41 @@
+from victory import victory as vic
+from use_functions import my_bill
+while True:
+    print('1.создать папку')
+    print('2.удалить файл/папку')
+    print('3.копировать файл/папку')
+    print('4.просмотр содержимого рабочей директории')
+    print('5.посмотреть только папки')
+    print('6.посмотреть только файлы')
+    print('7.просмотр информации об операционной системе')
+    print('8.создатель программы')
+    print('9.играть в викторину')
+    print('10.мой банковский счет')
+    print('11.смена рабочей директории')
+    print('12.выход')
+
+    choice = int(input('Выберите пункт меню'))
+    if choice == 1:
+        pass
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    elif choice == 4:
+        pass
+    elif choice == 5:
+        pass
+    elif choice == 6:
+        pass
+    elif choice == 7:
+        pass
+    elif choice == 8:
+        pass
+    elif choice == 9:
+        pass
+    elif choice == 10:
+        pass
+    elif choice == 11:
+        pass
+    else:
+        break
